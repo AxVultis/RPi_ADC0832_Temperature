fd57d4250f5092258aa877b0e7ec825f119f3abb
AxVultis <axvultis@gmail.com>
2019-10-26 15:26:18 -0400

    Create function to read the temperature

--- read_temperature.py
+++ read_temperature.py
@@ -10,13 +10,18 @@ def init():
 
 def loop():
     while True:
-        analog_val = ADC0832.get_result()
-        voltage = 5 * float(analog_val) / 255
-        rt = 10000 * voltage / (5 - voltage)
-        temp = 1 / (((math.log(rt / 10000)) / 3950) + (1 / (273.15 + 25)))
-        temp = temp - 273.15
-        print('temperature = %.2f C' % temp)
         time.sleep(0.2)
+        temp = get_temperature()
+        print('temperature = %.2f °C' % temp)
+
+
+def get_temperature():
+    analog_val = ADC0832.get_result()
+    voltage = 5 * float(analog_val) / 255
+    rt = 10000 * voltage / (5 - voltage)
+    temp = 1 / (((math.log(rt / 10000)) / 3950) + (1 / (273.15 + 25)))
+    temp = temp - 273.15
+    return temp
 
 
 if __name__ == '__main__':
